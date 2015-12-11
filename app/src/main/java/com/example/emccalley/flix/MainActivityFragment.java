59f096c8d7d0aaca6113f281d25e080096c2ab37
Estevan McCalley <estevan.mccalley@gmail.com>
2015-12-11 13:35:42 -0600

    Populate grid view with dummy movie poster images

--- app/src/main/java/com/example/emccalley/flix/MainActivityFragment.java
+++ app/src/main/java/com/example/emccalley/flix/MainActivityFragment.java
@@ -1,14 +1,15 @@
 package com.example.emccalley.flix;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,33 +25,65 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Dummy data
-        ArrayList<String> dummyArray = new ArrayList<String>();
-        dummyArray.add("First Movie");
-        dummyArray.add("Second Movie");
-        dummyArray.add("Third Movie");
-        dummyArray.add("Fourth Movie");
-        dummyArray.add("Fifth Movie");
-        dummyArray.add("Sixth Movie");
-        dummyArray.add("Seventh Movie");
-        dummyArray.add("Eighth Movie");
-        dummyArray.add("Ninth Movie");
-        dummyArray.add("Tenth Movie");
-
-        // The ArrayAdapter will take data from a source and
-        // use it to populate the ListView it's attached to.
-        mMovieAdapter = new ArrayAdapter<String>(
-                getActivity(), // The current context (this activity)
-                R.layout.list_item_movie, // The name of the layout ID.
-                R.id.list_item_movie_textview, // The ID of the textview to populate.
-                dummyArray);
-
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // Get a reference to the ListView, and attach this adapter to it.
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_movies);
-        listView.setAdapter(mMovieAdapter);
+        // Get a reference to the GridView, and attach this adapter to it.
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridView.setAdapter(new ImageAdapter(getContext()));
 
         return rootView;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(400, 600));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(0, 0, 0, 0);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.fightclub1, R.drawable.fightclub2,
+                R.drawable.fightclub3, R.drawable.fightclub4,
+                R.drawable.fightclub5, R.drawable.fightclub6,
+                R.drawable.fightclub1, R.drawable.fightclub2,
+                R.drawable.fightclub3, R.drawable.fightclub4,
+                R.drawable.fightclub5, R.drawable.fightclub6,
+                R.drawable.fightclub1, R.drawable.fightclub2,
+                R.drawable.fightclub3, R.drawable.fightclub4,
+                R.drawable.fightclub5, R.drawable.fightclub6,
+                R.drawable.fightclub1, R.drawable.fightclub2,
+                R.drawable.fightclub3, R.drawable.fightclub4,
+                R.drawable.fightclub5, R.drawable.fightclub6,
+        };
     }
 }
